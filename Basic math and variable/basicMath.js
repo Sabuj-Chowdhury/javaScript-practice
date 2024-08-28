ce0f221499317e91b1003e788b9866b635e8c45d
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-08-28 22:46:22 +0530

    basic addition

---
+++ Basic math and variable/basicMath.js
@@ -0,0 +1,8 @@
+let orange = 300;
+let apple = 400;
+let money = 1000;
+console.log("Apple price(1kg): "+apple);
+console.log("Orange price(1kg): "+orange);
+console.log("Money I have: "+money);
+console.log("Total money I have spent: "+ (apple+orange))
+console.log("shopkeeper will return: "+(money-(orange+apple)));
