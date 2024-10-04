0b652d20e8e3cebc4022d574ac81835ae1584632
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-10-05 03:18:06 +0530

    nested object acces

---
+++ ES6/map.js
@@ -0,0 +1,5 @@
+const arr = [3, 4, 5, 6, 67, 8];
+const double = (e) => e * 2;
+
+console.log(arr);
+console.log(arr.map(double));
--- ES6/practice/3.js
+++ ES6/practice/3.js
@@ -1,13 +1,30 @@
-// not correct solution
-const complexArrow = (arr) => {
-  let mul = 1;
-  for (const i in arr) {
-    // sum = sum + i * i;
-    mul = i * i;
+// still not correct
+const numArr = [1, 2, 34, 45, 6, 79];
+const fun = (arr) => {
+  const squr = (e) => e * e;
+  const sqrArr = arr.map(squr);
+  let sum = 0;
+  for (const i of sqrArr) {
+    sum += i;
   }
-  //   const avg = sum / arr.length;
-  //   return avg;
-  return mul;
+  const avg = sum / arr.length;
+  return avg;
 };
-const num = [2, 4];
-console.log(complexArrow(num));
+const result = fun(numArr);
+console.log(numArr);
+console.log(result);
+//  1 4 1156 36 6241
+// not correct solution
+// const complexArrow = (arr) => {
+//   // let mul = 1;
+//   let sum = 0;
+//   for (const i in arr) {
+//     sum = sum + i ** 2;
+//     // mul = i * i;
+//   }
+//   const avg = sum / arr.length;
+//   return avg;
+//   // return mul;
+// };
+// const num = [2, 4];
+// console.log(complexArrow(num));
